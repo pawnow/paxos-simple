ab61504407de3a4a45a43afe7095ce8ed620731b
Pawel Nowotarski <pawnow6@gmail.com>
2016-11-01 16:29:04 +0100

    Removed unused imports

--- src/main/java/pl/edu/agh/iosr/cdm/Proposal.java
+++ src/main/java/pl/edu/agh/iosr/cdm/Proposal.java
@@ -2,12 +2,8 @@ package pl.edu.agh.iosr.cdm;
 
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Data
