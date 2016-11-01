14a5481951630847919b74742cf96db111e8047f
domin4815 <domin4815@gmail.com>
2016-11-01 15:38:36 +0100

    Acceptor based on data in DB

--- src/main/java/pl/edu/agh/iosr/cdm/ProposalRepository.java
+++ src/main/java/pl/edu/agh/iosr/cdm/ProposalRepository.java
@@ -1,11 +1,17 @@
 package pl.edu.agh.iosr.cdm;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 /**
  * Created by domin4815 on 31.10.16.
  */
 @Repository
 public interface ProposalRepository extends CrudRepository<Proposal, Long> {
+    @Query("select p from Proposal p where p.id = " +
+            " (select max(pp.id) from Proposal pp) ")
+    Optional<Proposal> getByMaxId();
 }
