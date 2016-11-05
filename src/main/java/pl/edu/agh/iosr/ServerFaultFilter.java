b01f5ddfada470a28819abec90083d5a09379de3
domin4815 <domin4815@gmail.com>
2016-11-05 16:30:05 +0100

    Fix server after failure URL was also blocked xD

--- src/main/java/pl/edu/agh/iosr/ServerFaultFilter.java
+++ src/main/java/pl/edu/agh/iosr/ServerFaultFilter.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.filter.DelegatingFilterProxy;
 import pl.edu.agh.iosr.service.FaultService;
 
-import javax.servlet.*;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 /**
@@ -18,7 +22,12 @@ public class ServerFaultFilter extends DelegatingFilterProxy {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
 
-        if(faultService.isDown()){
+        String url = "";
+        if (servletRequest instanceof HttpServletRequest) {
+            url = ((HttpServletRequest)servletRequest).getRequestURL().toString();
+        }
+
+        if(faultService.isDown() && !url.contains("util")){
             throw new RuntimeException("Node is down");
         } else {
             filterChain.doFilter(servletRequest, servletResponse);
