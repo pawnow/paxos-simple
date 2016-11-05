86f79e1fb9eceaceb7375fdcacdbafffb653c744
Szymon Kopacz <sz.kopacz@gmail.com>
2016-11-05 14:09:45 +0100

    Initial changes to proposer

--- src/main/java/pl/edu/agh/iosr/controller/ProposerController.java
+++ src/main/java/pl/edu/agh/iosr/controller/ProposerController.java
@@ -1,18 +1,63 @@
 package pl.edu.agh.iosr.controller;
 
+import com.google.common.collect.Lists;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.edu.agh.iosr.cdm.NodesRegistryRepository;
+import pl.edu.agh.iosr.cdm.Node;
 import pl.edu.agh.iosr.cdm.Proposal;
+import pl.edu.agh.iosr.cdm.ProposalRepository;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 
 @RequestMapping("/proposer")
 @RestController
 public class ProposerController {
 
+    final static Logger logger = LoggerFactory.getLogger(ProposerController.class);
+
+    @Autowired
+    private NodesRegistryRepository nodesRegistryRepository;
+
+    @Autowired
+    private ProposalRepository proposalRepository;
+
     private static long id = 0;
+
+    private HashMap<Long, HashMap<Node, Boolean>> quorums = new HashMap<Long,  HashMap<Node, Boolean>>();
 
     @RequestMapping("/propose")
     public Proposal propose() {
-        //TODO: send proposal to quorum, receive response, send accept
-        return Proposal.builder().id(id++).build();
+
+        quorums.put(new Long(id++), getMinimalQuorum());
+        logger.debug("created proposal with id: " + id);
+        return Proposal.builder().id(id).build();
+
+    }
+
+    private HashMap<Node, Boolean> getMinimalQuorum(){
+        List<Node> nodes = Lists.newArrayList(nodesRegistryRepository.findAll());
+        Collections.shuffle(nodes);
+        List<Node> nodesHalf = nodes.subList(0, (nodes.size()%2));
+        HashMap<Node, Boolean> quorum = new HashMap<Node, Boolean>();
+        for(Node node : nodesHalf){
+            quorum.put(node, false);
+        }
+        return quorum;
+    }
+
+    private HashMap<Node, Boolean> getFullQuorum(){
+        List<Node> nodes = Lists.newArrayList(nodesRegistryRepository.findAll());
+        Collections.shuffle(nodes);
+        HashMap<Node, Boolean> quorum = new HashMap<Node, Boolean>();
+        for(Node node : nodes){
+            quorum.put(node, false);
+        }
+        return quorum;
     }
 }
