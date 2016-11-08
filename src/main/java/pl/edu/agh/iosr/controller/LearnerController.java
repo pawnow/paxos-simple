420ec68de50d1bfce80199784fde1b7d0920dc70
Szymon Kopacz <sz.kopacz@gmail.com>
2016-11-08 09:11:45 +0100

    Changed learner logic so it saves the value and changed tests accordingly

--- src/main/java/pl/edu/agh/iosr/controller/LearnerController.java
+++ src/main/java/pl/edu/agh/iosr/controller/LearnerController.java
@@ -2,10 +2,13 @@ package pl.edu.agh.iosr.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import pl.edu.agh.iosr.cdm.AcceptedProposal;
+import pl.edu.agh.iosr.cdm.AcceptedProposalRepository;
 import pl.edu.agh.iosr.cdm.Proposal;
 
 import java.util.Optional;
@@ -14,18 +17,23 @@ import java.util.Optional;
 @RestController
 public class LearnerController {
 
-    private Optional<Proposal> learnedProposal = Optional.empty();
+    @Autowired
+    AcceptedProposalRepository acceptedProposalRepository;
+
+    private Optional<AcceptedProposal> learnedProposal = Optional.empty();
 
     final static Logger logger = LoggerFactory.getLogger(LearnerController.class);
 
     @RequestMapping(method = RequestMethod.POST, value = "/learn")
     public void learn(@RequestBody Proposal proposal) {
         logger.info("Learned new value: " + proposal);
-        learnedProposal = Optional.ofNullable(proposal);
+        AcceptedProposal acceptedProposal = new AcceptedProposal(proposal);
+        acceptedProposalRepository.save(acceptedProposal);
+        learnedProposal = Optional.ofNullable(acceptedProposal);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/learn")
-    public Proposal getLearnedValue() {
-        return learnedProposal.orElseGet(() -> Proposal.builder().build());
+    public AcceptedProposal getLearnedValue() {
+        return learnedProposal.orElseGet(() -> AcceptedProposal.builder().build());
     }
 }
